dcc40166b9f8dcee008e06127d247d3429bc8461
Justin Weiss <justinweiss@Justins-MacBook-Pro.local>
2018-03-18 15:01:28 -0400

    Basis Set Up of TableView

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Justin Weiss on 3/18/18.
+//  Copyright © 2018 Justin Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
