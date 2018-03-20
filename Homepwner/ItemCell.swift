a42a971f4c2becfe23c57161955fc93bf699a1dc
Justin Weiss <justinweiss@Justins-MacBook-Pro.local>
2018-03-20 16:19:44 -0400

    Chapter 11 Finished without Challenges.    Chapter 12: Creating ItemCell.

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Justin Weiss on 3/20/18.
+//  Copyright © 2018 Justin Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
